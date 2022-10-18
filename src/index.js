9f08c248283c808379fdd126b9532b809740b5dd
Aaron David <ajdavid128@gmail.com>
2022-10-18 10:03:16 -0500

    add fetch request, toyRender function, added div, h2, img elements and appended

--- src/index.js
+++ src/index.js
@@ -13,3 +13,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const toyCollection = document.querySelector('#toy-collection')
+
+fetch("http://localhost:3000/toys")
+.then(response => response.json())
+.then(toyData => toyData.forEach(toyData => toyRender(toyData)))
+
+function toyRender(toy){
+  const toyDiv = document.createElement('div')
+  toyDiv.classList.add("card")
+
+  const h2 = document.createElement('h2')
+  h2.innerText = toy.name
+
+  const img = document.createElement('img')
+  img.classList.add("toy-avatar")
+  img.src = toy.image
+  
+
+  toyCollection.append(toyDiv)
+  toyDiv.append(h2)
+  toyDiv.append(img)
+  // const currentToy = toyData
+  // console.log(toyDiv)
+  // console.log(toyData[0].name)
+}
